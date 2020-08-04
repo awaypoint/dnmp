11af85902d2a0e580f262fb264ad241f01ead046
amoj <215089670@qq.com>
2020-08-04 16:06:19 +0800

    myself

---
+++ script/localBakMysql.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+scp -r 10.211.55.5:/mnt/data/www/dnmp/data/mysql/mysql-bin.\* /Users/away/Documents/backup/
+scp -r 10.211.55.5:/mnt/data/www/dnmp/backup/mysql.sql /Users/away/Documents/backup/
