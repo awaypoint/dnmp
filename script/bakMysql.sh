0ca28c71bf768488b5c30b4052cdeadf769ef6c4
amoj <215089670@qq.com>
2020-08-04 16:21:44 +0800

    密码写在配置文件

--- script/bakMysql.sh
+++ script/bakMysql.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 w=$(date +%w)
-pwd='123456'
 FilePath=/mnt/data/www/dnmp/data/mysql/
 targetPath=/mnt/data/www/dnmp/backup
 day=10
-if [ $w -eq 2 ]
+if [ $w -eq 0 ]
 then
     for db in test
     do
         echo 'full'
-        docker exec -it mysql mysqldump -uroot -p$pwd --quick --events  --databases $db --flush-logs --single-transaction > $targetPath/$db.sql
+        #docker exec -it mysql mysqldump --quick --events  --databases $db --flush-logs --single-transaction > $targetPath/$db.sql
     done
 else
    echo 'increase'
-   docker exec -it mysql mysqladmin -uroot -p$pwd flush-logs
+   docker exec -it mysql mysqladmin flush-logs
 fi
 find $FilePath -mtime +$day -name mysql-bin.\* -exec rm -rf {} \;
